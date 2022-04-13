be3489cfb140534fa0332328032c73a1541e143d
Akash Raman <akashjayaraman8841@gmail.com>
2022-04-13 07:36:28 +0530

    nothing

--- script.js
+++ script.js
@@ -133,21 +133,22 @@ class App {
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
-
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
     if (type === 'running') {
       const cadence = +inputCadence.value;
       if(!validInputs(distance,duration,cadence)||!allPositive(distance,duration,cadence)) return alert("Enter positive input");
+      workout = new Running([lat, lng],distance,duration,cadence);
     }
     
     if (type === 'cycling') {
       const elevation = +inputElevation.value;
       if(!validInputs(distance,duration,elevation)||!allPositive(distance,duration)) return alert("Enter positive input");
+      workout = new Cycling([lat, lng],distance,duration,elevation);
     }
-
     
+    console.log(workout);
 
-    const { lat, lng } = this.#mapEvent.latlng;
-    
     L.marker([lat, lng], { icon: mappyIcon })
       .addTo(this.#map)
       .bindPopup(
